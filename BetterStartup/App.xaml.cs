cd8d4dd2da5db1722b5a74d02ad6c8d3f5fa8bf3
felip <felip@DESKTOP-2A9IAT7>
2024-04-16 08:27:54 -0300

    Add project files.

---
+++ BetterStartup/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BetterStartup
+    {
+    public partial class App : Application
+        {
+        public App()
+            {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+            }
+        }
+    }
---
+++ BetterStartup/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace BetterStartup
+    {
+    public partial class MainPage : ContentPage
+        {
+        int count = 0;
+
+        public MainPage()
+            {
+            InitializeComponent();
+            }
+
+
+        }
+
+    }
